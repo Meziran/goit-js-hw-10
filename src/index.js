4a3358d0f57f7f5d3b55a11cf618b8f65d18ea20
Mateichuk_Oleksandr <71691100+Meziran@users.noreply.github.com>
2021-06-19 21:36:29 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './sass/main.scss';
+
+import './styles.css';
+import menuEl from './menu.json';
+import MenuTmp from './templates/gallery.hbs';
+
+const refs = {
+    menu: document.querySelector('.js-menu'),
+}; 
+
+const menuMarkUp = MenuTmp([...menuEl]);
+refs.menu.insertAdjacentHTML('beforeend', menuMarkUp);
